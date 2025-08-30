e3264d57bcd5e5d5d3cbe30cdd996445f5b99277
Mokatil Dev <mokatildev@gmail.com>
2025-08-30 14:06:18 +0100

    add material icon

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,3 +1,22 @@
 return {
   "DaikyXendo/nvim-material-icon",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-web-devicons").setup({
+
+      override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh",
+        },
+      },
+
+      color_icons = true,
+      default = true,
+    })
+  end,
 }
