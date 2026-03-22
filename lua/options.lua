ca7fe20a01d9991899c50d71c03876fb528eae40
Mokatil Dev <mokatildev@gmail.com>
2026-03-22 15:21:21 +0100

    Add spelling

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,4 @@
-require "nvchad.options"
+require("nvchad.options")
 
 local o = vim.o
 o.shiftwidth = 4
@@ -6,14 +6,17 @@ o.tabstop = 4
 o.softtabstop = 4
 
 vim.g.vscode_snippets_path = "~/.vscode/extensions/pulkitgangwar.nextjs-snippets-1.0.3"
-vim.filetype.add {
-  extension = {
-    postcss = "css",
-  },
-  filename = {
-    ["postcss.config.js"] = "javascript",
-  },
-}
-vim.notify = require "notify"
+vim.filetype.add({
+	extension = {
+		postcss = "css",
+	},
+	filename = {
+		["postcss.config.js"] = "javascript",
+	},
+})
+
+vim.notify = require("notify")
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
