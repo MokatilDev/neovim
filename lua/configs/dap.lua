c487a36a0d2d72ed150e8cac028663a9121fbb77
Mokatil Dev <mokatildev@gmail.com>
2026-07-16 18:17:38 +0100

    feat(dap): add debug configurations for go, lua, cpp, and python

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -22,3 +22,69 @@ mason_dap.setup({
 		end,
 	},
 })
+
+dap.configurations.go = {
+	{
+		type = "delve",
+		name = "Debug",
+		request = "launch",
+		showLog = false,
+		program = "${file}",
+		dlvToolPath = vim.fn.exepath("dlv"),
+	},
+	{
+		type = "delve",
+		name = "Debug Test",
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+		dlvToolPath = vim.fn.exepath("dlv"),
+	},
+}
+
+dap.configurations.lua = {
+	{
+		name = "Current file (local-lua-dbg, lua)",
+		type = "local-lua",
+		request = "launch",
+		cwd = "${workspaceFolder}",
+		program = {
+			lua = "lua",
+			file = "${file}",
+		},
+		args = {},
+	},
+}
+
+dap.configurations.cpp = {
+	{
+		name = "Launch",
+		type = "codelldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+	},
+}
+
+dap.configurations.python = {
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		pythonPath = function()
+			local cwd = vim.fn.getcwd()
+			if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+				return cwd .. "/venv/bin/python"
+			elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+				return cwd .. "/.venv/bin/python"
+			else
+				return "/usr/bin/python"
+			end
+		end,
+	},
+}
