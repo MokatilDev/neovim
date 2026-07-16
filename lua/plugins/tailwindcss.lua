433493e1f16556196db46af376e34b6bfe65e004
Mokatil Dev <mokatildev@gmail.com>
2026-07-16 18:31:06 +0100

    refactor: update config settingsd

--- lua/plugins/tailwindcss.lua
+++ lua/plugins/tailwindcss.lua
@@ -1,24 +1,31 @@
 return {
-  -- {
-  --   "razak17/tailwind-fold.nvim",
-  --   opts = {
-  --     min_chars = 50,
-  --   },
-  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  --   ft = { "html", "svelte", "astro", "vue", "typescriptreact" },
-  -- },
+	{
+		"laytan/tailwind-sorter.nvim",
+		cmd = {
+			"TailwindSort",
+			"TailwindSortOnSaveToggle",
+		},
 
-  {
-    "laytan/tailwind-sorter.nvim",
-    cmd = {
-      "TailwindSort",
-      "TailwindSortOnSaveToggle",
-    },
-    keys = {
-      { "<Leader>cS", "<CMD>TailwindSortOnSaveToggle<CR>", desc = "toggle Tailwind CSS classes sort on save" },
-    },
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-    build = "cd formatter && npm i && npm run build",
-    config = true,
-  },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+		build = "cd formatter && npm ci && npm run build",
+
+		config = function()
+			require("tailwind-sorter").setup({
+				on_save_enabled = true,
+				on_save_pattern = {
+					"*.html",
+					"*.js",
+					"*.jsx",
+					"*.tsx",
+					"*.twig",
+					"*.hbs",
+					"*.php",
+					"*.heex",
+					"*.astro",
+				},
+				node_path = "node",
+				trim_spaces = true,
+			})
+		end,
+	},
 }
