49d8a8cc46c231f20decafbf7071611c39dd0880
Mokatil Dev <mokatildev@gmail.com>
2026-07-16 18:29:15 +0100

    refactor: update view settings and file exclusions

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,18 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    enabled = true,
-    opts = {
-      view = {
-        side = "left",
-        width = 35,
-      },
-    },
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		enabled = true,
+		opts = {
+			view = {
+				side = "left",
+				width = 30,
+			},
+			filters = {
+				dotfiles = true,
+				exclude = {
+					".env",
+				},
+			},
+		},
+	},
 }
