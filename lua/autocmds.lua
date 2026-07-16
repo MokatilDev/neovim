a2f7fb84c969ac4734b843c8268b4de34b3eecba
Mokatil Dev <mokatildev@gmail.com>
2026-07-16 18:13:13 +0100

    feat(nvim): add autocommands for DBUI and docker

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,38 @@
-require "nvchad.autocmds"
+require("nvchad.autocmds")
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "dbout",
+	callback = function()
+		vim.opt_local.foldenable = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "yaml", "docker-compose" },
+	callback = function()
+		require("cmp").setup.buffer({
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+			},
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "sql", "mysql", "plsql", "postgresql" },
+	callback = function()
+		require("cmp").setup.buffer({
+			sources = {
+				{ name = "vim-dadbod-completion" },
+				{ name = "buffer" },
+			},
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
